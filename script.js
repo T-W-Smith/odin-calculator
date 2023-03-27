2dbdda873f446746e349c1b87c90d242b03b1fd7
T-W-Smith <slicksmith14@gmail.com>
2023-03-27 17:49:55 -0400

    Add a main operation function

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let numOne;
+let numTwo;
+let operator;
+
+function operate(numOne, numTwo, operator){
+    switch (operator){
+        case '+':
+            add(numOne, numTwo);
+            break;
+        case '-':
+            subtract(numOne, numTwo);
+            break;
+        case '*':
+            multiply(numOne, numTwo);
+            break;
+        case '/':
+            divide(numOne, numTwo);
+            break;
+        default:
+            console.log('Invalid operator');
+    }
+}
+
 function add(num1, num2){
     newNum = num1 + num2;
     console.log(newNum);
