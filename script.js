2f13e14cf875c190321c918bf5002c16fc8b1df9
T-W-Smith <slicksmith14@gmail.com>
2023-03-27 17:39:03 -0400

    Add HTML boilerplate and basic calculator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(num1, num2){
+    newNum = num1 + num2;
+    console.log(newNum);
+}
+
+function subtract(num1, num2){
+    newNum = num1 - num2;
+    console.log(newNum);
+}
+
+function multiply(num1, num2){
+    newNum = num1 * num2;
+    console.log(newNum);
+}
+
+function divide(num1, num2){
+    newNum = num1 / num2;
+    console.log(newNum);
+}
