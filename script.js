9d3c6a2008309d9b9488cb07dffa1b47f62e19cc
T-W-Smith <slicksmith14@gmail.com>
2023-04-05 17:25:14 -0400

    Make the calculator actually function

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const calcDisplay = document.getElementById('display');
 const numButtons = document.getElementById('numButtons');
 const opButtons = document.getElementById('opButtons');
 const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
 
 numButtons.addEventListener('click', (e) => {
     const isButton = e.target.nodeName === 'BUTTON';
-    if (!isButton)
-        return
+    if (!isButton || e.target.id === 'equals')
+        return;
     else
         calcDisplay.textContent += e.target.textContent;
 });
@@ -18,29 +19,38 @@ numButtons.addEventListener('click', (e) => {
 opButtons.addEventListener('click', (e) => {
     const isButton = e.target.nodeName === 'BUTTON';
     if (!isButton)
-        return
+        return;
     else
-        calcDisplay.textContent += e.target.textContent;
+        numOne = Number(calcDisplay.textContent);
+        operator = e.target.textContent;
+        clearDisplay();
 });
 
-clearButton.addEventListener('click', (e) => {
-    calcDisplay.textContent = ' ';
+clearButton.addEventListener('click', clearDisplay);
+
+equalsButton.addEventListener('click', (e) => {
+    numTwo = Number(calcDisplay.textContent);
+    clearDisplay();
+    operate(numOne, numTwo, operator)
 })
 
+function clearDisplay(){
+    calcDisplay.textContent = '';
+}
 
-function operate(numOne, numTwo, operator){
-    switch (operator){
+function operate(one, two, op){
+    switch (op){
         case '+':
-            add(numOne, numTwo);
+            add(one, two);
             break;
         case '-':
-            subtract(numOne, numTwo);
+            subtract(one, two);
             break;
         case '*':
-            multiply(numOne, numTwo);
+            multiply(one, two);
             break;
         case '/':
-            divide(numOne, numTwo);
+            divide(one, two);
             break;
         default:
             console.log('Invalid operator');
@@ -49,20 +59,20 @@ function operate(numOne, numTwo, operator){
 
 function add(num1, num2){
     newNum = num1 + num2;
-    console.log(newNum);
+    calcDisplay.textContent = newNum;
 }
 
 function subtract(num1, num2){
     newNum = num1 - num2;
-    console.log(newNum);
+    calcDisplay.textContent = newNum;
 }
 
 function multiply(num1, num2){
     newNum = num1 * num2;
-    console.log(newNum);
+    calcDisplay.textContent = newNum;
 }
 
 function divide(num1, num2){
     newNum = num1 / num2;
-    console.log(newNum);
+    calcDisplay.textContent = newNum;
 }
