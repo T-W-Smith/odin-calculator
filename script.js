95c2ae7229c67433da63bc108c3aa06ca0edd1d4
T-W-Smith <slicksmith14@gmail.com>
2023-04-24 17:47:17 -0400

    cleaned/refactored code

--- script.js
+++ script.js
@@ -6,28 +6,34 @@ let completedOp = false;
 
 const currentDisplay = document.getElementById('currentDisplay');
 const prevDisplay = document.getElementById('prevDisplay');
-const numButtons = document.getElementById('numButtons');
-const opButtons = document.getElementById('opButtons');
+const numButtons = document.querySelectorAll('.numButton');
+const opButtons = document.querySelectorAll('.opButton');
 const clearButton = document.getElementById('clear');
 const deleteButton = document.getElementById('delete');
 const equalsButton = document.getElementById('equals');
 
-numButtons.addEventListener('click', (e) => {
-    if (currentDisplay.textContent === '0' || refreshScreen){
-        refresh();
-    }
-    currentDisplay.textContent += e.target.textContent;
+numButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (currentDisplay.textContent === '0' || refreshScreen){
+            refresh();
+        }
+        const numValue = button.getAttribute('data-num');
+        currentDisplay.textContent += numValue;
+    })
 });
 
-opButtons.addEventListener('click', (e) => {
-    if (!currentDisplay.textContent || e.target.id === 'equals')
-        return;
-    else
-        operator = e.target.textContent;
-        numOne = Number(currentDisplay.textContent);
-        refreshScreen = true;
-        completedOp = false;
-        prevDisplay.textContent = numOne + ' ' + operator;
+opButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (!currentDisplay.textContent)
+            return;
+        else
+            opValue = button.getAttribute('data-num');
+            operator = opValue;
+            numOne = Number(currentDisplay.textContent);
+            refreshScreen = true;
+            completedOp = false;
+            prevDisplay.textContent = numOne + ' ' + operator;
+    })
 });
 
 clearButton.addEventListener('click', () => {
